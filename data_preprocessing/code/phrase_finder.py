47ebc44566fcb48645411a2fc8a73556931a99cf
Shankeerthan95 <shankeerthan1995@gmail.com>
2019-09-03 18:34:21 +0530

    added storey.csv

--- data_preprocessing/code/phrase_finder.py
+++ data_preprocessing/code/phrase_finder.py
@@ -27,7 +27,7 @@ def compare_str(tokens, word):
 def analyse_word(path, word=''):
     df = pd.read_csv(path)
     print()
-    l_of_title_desc = [ (row[0], tokenize_text(row[1] + '  ' + row[5])) for (index, row) in df.iterrows() if index != 0];
+    l_of_title_desc = [ (row[0], tokenize_text(row[1] + '  ' + row[5])) for (index, row) in df.iterrows() if row[0] != 'ID'];
     # l_of_title_desc = [list(set(title_desc))    for title_desc in l_of_title_desc];
     phrases = []
     for (key ,ti_des) in l_of_title_desc:
@@ -35,23 +35,67 @@ def analyse_word(path, word=''):
             i = compare_str(ti_des, word);
             phrase = ''
             if i == -23:
-                phrases.append((key, '          empty          '))
+                phrases.append((key, '1'))
                 continue
-            elif (i == 0):
-                phrase = ti_des[i] + '  ' + ti_des[i+1]
             else:
-                phrase = ti_des[i-1] + '    ' + ti_des[i] + '  ' + ti_des[i+1]
+                phrase = ti_des[i-1] + '  ' + ti_des[i]
+            # elif (i == 0):
+            #     phrase = ti_des[i] + '  ' + ti_des[i+1]
+            # else:
+            #     phrase = ti_des[i-1] + '    ' + ti_des[i] + '  ' + ti_des[i+1]
             phrases.append((key, phrase ))
         except IndexError:
+            phrases.append((key, ti_des[i]))
             pass
     
-    for phrase in phrases:
-        print(phrase[0], '    :   ' , phrase[1]) 
+    # for phrase in phrases:
+    #     print(phrase[0], '    :   ' , phrase[1]) 
 
     print()
     print('Number of houses: ', len(phrases))
+    return phrases
 
 
 
+related_words = {
+    'storey': ['storied', 'storyed', 'story',
+                'storey', 'stories', 'storeid', 'storie'],
+    'one': ['single', 'one','1', 'singal', 'sigle'],
+    'two': ['two', '2', 'tow', 'double'],
+    'three': ['three', '3'],
+    'four': ['four', '4']
+}
 
-analyse_word('../resource/new.csv', sys.argv[1])
+def isContains(text, l):
+    for w in l:
+        if text.find(w) != -1:
+            return True
+    return False 
+
+
+
+phrases = analyse_word('../resource/new.csv', sys.argv[1])
+# for (key, i) in phrases:
+#     print(key, '   :   ', i)
+li = []
+for (key, w) in phrases:
+    if isContains(w, related_words['storey']):
+        if w == '1': 
+            li.append([key, 1])
+        elif isContains(w, related_words['one']):
+            li.append([key, 1])
+        elif isContains(w, related_words['two']):
+            li.append([key, 2])
+        elif isContains(w, related_words['three']):
+            li.append([key, 3])
+        elif isContains(w, related_words['four']):
+            li.append([key, 4])
+        else:
+            li.append([key, -1])
+    else:
+        li.append([key, 1])
+# print(li)
+df = pd.DataFrame(li) 
+df.to_csv('../resource/storey.csv', index=False);  
+
+
