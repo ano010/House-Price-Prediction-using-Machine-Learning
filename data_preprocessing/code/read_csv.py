7946ed513494cc6b96f33f638a247be7187633fe
Anojan <anojan010@gmail.com>
2019-10-17 09:52:40 +0530

    incorrect values and missing values were corrected in the data set

--- data_preprocessing/code/read_csv.py
+++ data_preprocessing/code/read_csv.py
@@ -1,37 +1,9 @@
-import pandas
+import pandas as pd
 import csv
 
 
-df = pandas.read_csv('houses.csv')
-
-print(df)
-
-my_list = []
+df = pd.read_csv('../resource/data_set_535_ano_sankee.csv')
 
 for index, row in df.iterrows():
-    if(row[6].isnumeric()):
-        beds = row[6]
-        baths = row[7]
-        house_size = row[8]
-        land_size = row[9]
-        row[6] = ''
-        row[7] = beds
-        row[8] = baths
-        row[9] = house_size
-        row[10] = land_size
-    my_list.append(row)
-my_dataframe = pandas.DataFrame(my_list)
-my_dataframe.to_csv('new.csv', index=False)
-
-# row = [['2', ' Marie', ' California'], ['3', ' Marief',
-#                                         ' Californiaf'], ['3', ' Marief', ' Californiaf']]
-
-# with open('new.csv', 'w', newline='') as writeFile:
-#     writer = csv.writer(writeFile, delimiter=',')
-#     print(my_list[0])
-#     writer.writerows(my_list)
-
-# writeFile.close()
-
-# for element in my_list:
-#     print(element[9])
+    if not isinstance(row[0], float):
+        print(row[0])
