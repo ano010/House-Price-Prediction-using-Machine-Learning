f20d76abdeb20b9e94882225d8999a4f6208c6f3
Anojan <anojan010@gmail.com>
2019-10-19 08:08:13 +0530

    Outliers removed from the data set

--- data_preprocessing/code/read_csv.py
+++ data_preprocessing/code/read_csv.py
@@ -4,6 +4,10 @@ import csv
 
 df = pd.read_csv('../resource/data_set_535_ano_sankee.csv')
 
+# for index, row in df.iterrows():
+#     if not isinstance(row[0], float):
+#         print(row[0])
+
 for index, row in df.iterrows():
-    if not isinstance(row[0], float):
-        print(row[0])
+    if row[14] == 0 and row[14] != 1:
+        print(row[0])     
